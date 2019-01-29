f684003e04deab364296b0cf9a2c3a6b3b4bee35
שמואל כהן <tr.tuz2002@gmail.com>
2019-01-29 18:27:26 +0200

    Finished the project

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from random import randint
+from flask import Flask, request, redirect
+from flask import render_template
+import random
+#from database import *
+import requests
+import time
+
 app = Flask(__name__)
 
+source = ''
+index = 0
+	
 @app.route('/')
-def hello_world():
-    return 'Hello, World test!'
+def home():
 
+	with open("./templates/source.txt") as file:
+		source = file.read()
+		source = source.split('\n')
+	print(source)
+	random.shuffle(source)
+	marker = 0
+	score = 0
+	
+	return render_template("home.html", words = source, count = 3000, marker = marker, score = score)
+	
+@app.route('/results', methods = ['GET', 'POST'])
+def results():
+
+	if request.method == 'POST':
+		return render_template('results.html', rightWords = request.form['right'], wrongWords = request.form['wrong'])
+	return "oh no"
+	
 if __name__ == '__main__':
     app.run(debug=True)
-
